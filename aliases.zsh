20e3866e02e239ac5b44c673a6593dd5af66c123
akz92 <aride.moulin@gmail.com>
2018-07-31 00:42:55 +0100

    Add zsh aliases

---
+++ aliases.zsh
@@ -0,0 +1,43 @@
+alias git=hub
+alias ctags="`brew --prefix`/bin/ctags"
+alias dev="cd ~/dev"
+alias sz="source ~/.zshrc"
+alias zlt="/usr/bin/time zsh -i -c exit"
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias ionpi="ionic prepare ios"
+alias fixpsql="rm /usr/local/var/postgres/postmaster.pid && brew services restart postgresql"
+alias qutebrowser="open -a /Applications/qutebrowser.app --args --enable-webengine-inspector"
+alias wira="awc && ira"
+if [ -n "$TMUX" ]; then alias fzf="fzf-tmux"; fi
+
+# Vim aliases
+alias vim=$EDITOR
+alias vimpi="vim +PlugInstall"
+alias vimpc="vim +PlugClean"
+alias vimpu="vim +PlugUpdate"
+
+# Rails aliases
+alias rr="bundle exec rake routes"
+alias rrf="bundle exec rake routes | fzf"
+alias rs="bundle exec rails s -b 127.0.0.1"
+alias rc="bundle exec rails c"
+alias rdc="bundle exec rake db:create"
+alias rdd="bundle exec rake db:drop"
+alias rdm="bundle exec rake db:migrate"
+alias rds="bundle exec rake db:seed"
+alias rdsl="bundle exec rake db:schema:load"
+alias rdr="rdsl && rds"
+alias rgmi="rails g migration"
+alias rgmo="rails g model"
+
+# Heroku aliases
+alias hrc="heroku run bundle exec rails c"
+alias hrdm="heroku run bundle exec rake db:migrate"
+alias hrds="heroku run bundle exec rake db:seed"
+alias hrdsl="heroku run bundle exec rake db:schema:load"
+alias hl="heroku logs -t"
+alias hbc="heroku pg:backups:capture"
+alias hbd="heroku pg:backups:download"
+
